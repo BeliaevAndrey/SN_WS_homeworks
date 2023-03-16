6ce7e900469733c85a164242f904a53b4cafda5c
Andrei Beliaev <abbot190414@gmail.com>
2023-03-16 16:40:45 +0300

    Block01 - Hard - second and third tests for task03 are added

--- block_01/tests/Hard/test_task03.py
+++ block_01/tests/Hard/test_task03.py
@@ -6,3 +6,17 @@ from block_01.Hard.task03 import summation
 @pytest.mark.parametrize("a, b, c, d, e, expected", [(1, 2, 3, 4, 5, 10)])
 def test_summation_success_01(a, b, c, d, e, expected: int):
     assert expected == summation(a, b, c, d, e)
+
+
+@pytest.mark.parametrize("tpl, expected",
+                         [((1, 2, 3, 4, 5, 6, 7, 8, 9, 10), range(30, 35))
+                          ])
+def test_summation_success_02(tpl, expected):
+    assert summation(*tpl) in expected
+
+
+@pytest.mark.parametrize("tpl, dct, expected",
+                         [((1, 2, 3, 4, 5, 6, 7, 8, ),  {'f': 9, 'g':10, 'h': 11}, range(39, 43))
+                          ])
+def test_summation_success_03(tpl, dct, expected):
+    assert summation(*tpl, **dct) in expected
