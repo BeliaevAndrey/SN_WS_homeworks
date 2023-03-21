e264dd9053a0623d13c6ffb9f1898a8152d712ce
Andrei Beliaev <abbot190414@gmail.com>
2023-03-21 13:15:38 +0300

    Block02 - Medium - task02_02-04.py summation function with different calls added

--- block_02/Medium/task02_02-04.py
+++ block_02/Medium/task02_02-04.py
@@ -11,11 +11,11 @@ def outer_function():
 
 
 def main():
-    print(outer_function())         # subtask 02
-    result = outer_function()       # subtask 03
-    print(result)
-    print(result(1, 2, 3, 4, ))                 # subtask 04 - v1
-    print(outer_function()(1, 2, 3, 4, ))       # subtask 04 - v2
+    print(outer_function())         # subtask 02; output: <function outer_function.<locals>.summation at 0x7fbec6738ca0>
+    result = outer_function()       # subtask 03;
+    print(result)                   # output: <function outer_function.<locals>.summation at 0x7fbec6738ca0>
+    print(result(1, 2, 3, 4, ))                     # subtask 04 - v1; output: 10
+    print(outer_function()(1, 2, 3, 4, 5, ))        # subtask 04 - v2; output: 15
 
 
 if __name__ == '__main__':
